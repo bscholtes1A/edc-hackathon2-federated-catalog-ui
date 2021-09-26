f85a84b1c7e81cdbc7443b1b44c7d70d8d297d0a
bscholtes1A <88310985+bscholtes1A@users.noreply.github.com>
2021-09-26 17:50:48 +0200

    Create first draft version of catalog

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <div class="header-img">Federated</div>
+    <router-view />
+  </div>
+</template>
+<script>
+export default {
+  name: "App"
+};
+</script>
+<style>
+#app {
+  margin-top: 50px;
+}
+.search-wrapper {
+  margin: 20px;
+}
+.badge {
+  border-radius: 0;
+}
+.header-img {
+  width: 100%;
+  height: 400px;
+  background: url("./assets/multimodal.jpg");
+  background-size: cover;
+}
+html,
+body {
+  height: 100%;
+}
+body {
+  background-position: center center;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: cover;
+  background-color: rgb(237, 238, 250);
+}
+</style>
